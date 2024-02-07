5b27f2d9f2bfab8893fa9b86d1a326bc4000136e
userCelesto <jomaridelmundo@gmail.com>
2024-02-07 13:27:21 +0800

    updated login-signup page ui

--- login/index.php
+++ login/index.php
@@ -10,22 +10,48 @@ if (isset($_SESSION['admin']) || isset($_SESSION['customer'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../default.css">
     <link rel="stylesheet" href="style.css">
     <!-- <script src="https://unpkg.com/just-validate@latest/dist/just-validate.production.min.js" defer></script> -->
     <!-- <script src="./validation.js" defer></script> -->
 </head>
 <body>
-    <form action="login.php" method="POST" id="login-form" novalidate>
-        <?php if(isset($_SESSION['message'])) echo $_SESSION['message'] ?>
-        <div>
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email" autocomplete='off'>
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password">
-        </div>
-        <input type="submit" id="submit" name="submit" value="Log In">
-    </form>
-</body>
+  <main>
+    <div class="login-container">
+        <figure>
+          <img src="loginLogo.jpg" alt="" />
+        </figure>
+        <section class="login-section">
+            <hgroup>
+              <h1>Login</h1>
+              <p>Welcome back! Please login to your account.</p>
+            </hgroup>
+            <form action="login.php" method="POST" id="login-form" class="form" novalidate>
+              <?= $_SESSION['message'] ?? NULL ?>
+              <div class="input-field">
+                  <label class="label" for="email">Email</label>
+                  <input type="text" id="email" name="email" autocomplete='off'>
+              </div>
+              <div class="input-field">
+                  <label class="label" for="password">Password</label>
+                  <input type="password" id="password" name="password">
+              </div>
+
+              <div class="remember-me">
+                  <input id="remember-me" type="checkbox" />
+                  <h4>Remember Me</h4>
+                  <a href="#" class="Forgot">Forgot Password?</a>
+              </div>
+
+              <input type="submit" id="submit" name="submit" value="Log In">
+
+            </form>
+            <div class="signup">
+                <span>Don't have an account? </span><a href="../register/">Signup</a>
+            </div>
+        </section>
+      </div>
+  </main>
+
+  </body>
 </html>
--- register/index.php
+++ register/index.php
@@ -10,35 +10,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Create an account</title>
+    <link rel="stylesheet" href="../default.css">
     <link rel="stylesheet" href="style.css">
     <script src="https://unpkg.com/just-validate@latest/dist/just-validate.production.min.js" defer></script>
     <script src="./validation.js" defer></script>
 </head>
 <body>
-<h1>Signup</h1>
-<form method="POST" id="signup-form" novalidate>
-    <div>
-        <label for="first_name">First Name</label>
-        <input type="text" id="first_name" name="first_name" autocomplete='off'>
+    <div class="main-container">
+        <section class="section left-section">
+            <img src="loginLogo.jpg" alt="" />
+        </section>
+        <section class="section right-section">
+            <div action="" class="form-container">
+                <h1>Signup</h1>
+                <p>Hello There! Create an account and join Us!.</p>
+
+                <form method="POST" id="signup-form" novalidate>
+                    <div class="main-form">
+                        <div class="input-field" >
+                            <label class="label" for="first_name">First Name</label>
+                            <input type="text" id="first_name" name="first_name" autocomplete='off'>
+                        </div>
+                        <div class="input-field" >
+                            <label class="label" for="last_name">Last Name</label>
+                            <input type="text" id="last_name" name="last_name" autocomplete='off'>
+                        </div>
+                        <div class="input-field" >
+                            <label class="label" for="email">Email</label>
+                            <input type="email" id="email" name="email" autocomplete='off'>
+                        </div>
+                        <div class="input-field" >
+                            <label class="label" for="password">Password</label>
+                            <input type="password" id="password" name="password">
+                        </div>
+                        <div class="input-field" >
+                            <label class="label" for="password_confirmation">Confirm Password</label>
+                            <input type="password" id="password_confirmation" name="password_confirmation">
+                        </div>
+                    </div>
+                    <input id="loginBtn" type="submit" name="submit" value="Submit">
+                </form>
+                
+
+                <div class="signup">
+                    <span>Already have an account? </span><a href="../login/">Login</a>
+                </div>
+            </div>
+        </section>
     </div>
-    <div>
-        <label for="last_name">Last Name</label>
-        <input type="text" id="last_name" name="last_name" autocomplete='off'>
-    </div>
-    <div>
-        <label for="email">Email</label>
-        <input type="text" id="email" name="email" autocomplete='off'>
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password">
-    </div>
-    <div>
-        <label for="password_confirmation">Confirm Password</label>
-        <input type="password" id="password_confirmation" name="password_confirmation">
-    </div>
-    <input type="submit" name="submit" value="Submit">
-</form>
+    
 </body>
 </html>
